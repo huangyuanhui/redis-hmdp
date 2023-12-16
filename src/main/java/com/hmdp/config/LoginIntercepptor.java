1f57eda0322a9d8015ed9bd3f3a4138fe6116d62
huangyuanhui <2278181701@qq.com>
2023-12-16 22:23:27 +0800

    Redis方式：登录拦截校验及登录续期

--- src/main/java/com/hmdp/config/LoginIntercepptor.java
+++ src/main/java/com/hmdp/config/LoginIntercepptor.java
@@ -1,30 +1,56 @@
 package com.hmdp.config;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.SystemConstants.LOGIN_TOKEN_KEY;
+import static com.hmdp.utils.SystemConstants.LOGIN_TOKEN_TTL;
 
 /**
  * 登录拦截器
  */
 public class LoginIntercepptor implements HandlerInterceptor {
 
+    private StringRedisTemplate redisTemplate;
+
+    public LoginIntercepptor(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 获取Session保存的登录用户
-        Object user = request.getSession().getAttribute("user");
-        // 判断用户是否存在（存在即登录，不存在则没登录）
-        if (user == null) {
-            // 不存在，则拦截请求，返回401未授权状态码
+        // 获取请求头中的Token
+        String token = request.getHeader("authorization");
+        if (StrUtil.isBlank(token)) {
+            // token不存在，说明未登录，则拦截请求，返回401未授权状态码
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
+        }
+        // 基于token从Redis中获取用户
+        String key = LOGIN_TOKEN_KEY + token;
+        Map<Object, Object> userMap = redisTemplate.opsForHash().entries(key);
+        // 判断用户是否存在
+        if (userMap.isEmpty()) {
+            // 不存在，说明未登录，则拦截请求，返回401未授权状态码
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return false;
         }
         // 存在，将登录用户保存到ThreadLocal
-        UserHolder.saveUser((UserDTO) user);
+        UserDTO user = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
+        UserHolder.saveUser(user);
+        // 登录续期，刷新token有效期
+        redisTemplate.expire(key, LOGIN_TOKEN_TTL, TimeUnit.MINUTES);
         // 放行请求
         return true;
     }
--- src/main/java/com/hmdp/config/MvcConfig.java
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -1,16 +1,22 @@
 package com.hmdp.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
+    @Resource
+    private StringRedisTemplate redisTemplate;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 添加登录校验拦截器
-        registry.addInterceptor(new LoginIntercepptor())
+        registry.addInterceptor(new LoginIntercepptor(redisTemplate))
                 // 排除不需要拦截的请求路径，即不登陆也可以访问的资源
                 .excludePathPatterns(
                         "/user/code",
