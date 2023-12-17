3daacb023ea5860cef3b4cd9fef2e542219efa1d
huangyuanhui <2278181701@qq.com>
2023-12-18 00:31:26 +0800

    缓存工具类在测试用例中使用

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,10 +1,15 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.CacheClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -12,9 +17,13 @@ class HmDianPingApplicationTests {
     @Resource
     private ShopServiceImpl shopService;
 
+    @Resource
+    private CacheClient cacheClient;
+
     @Test
     public void testSaveShop() {
-        shopService.saveShop2Redis(1L, 30L);
+        Shop shop = shopService.getById(1L);
+        cacheClient.setWithLogicExpire(CACHE_SHOP_KEY + 1L, shop, 30L, TimeUnit.SECONDS);
     }
 
 }
