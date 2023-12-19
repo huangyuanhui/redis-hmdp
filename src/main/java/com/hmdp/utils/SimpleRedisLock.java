9d1f863d19cbe908c56ed4f6576a2f4806331017
huangyuanhui <2278181701@qq.com>
2023-12-19 21:05:59 +0800

    集群模式下一人一单线程安全问题：redis实现分布式锁改进一 删除锁的时候判断线程标识是否一致！

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,5 +1,6 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.BooleanUtil;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
@@ -10,11 +11,20 @@ import java.util.concurrent.TimeUnit;
  */
 public class SimpleRedisLock implements ILock {
 
-    // key的统一前缀
+    /**
+     * key的统一前缀
+     */
     private static final String KEY_PREFIX = "lock:";
 
+    /**
+     * 获取机器的UUID
+     */
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+
     private StringRedisTemplate stringRedisTemplate;
-    // 业务名
+    /**
+     * 锁名
+     */
     private String name;
 
     public SimpleRedisLock(StringRedisTemplate stringRedisTemplate, String name) {
@@ -27,20 +37,27 @@ public class SimpleRedisLock implements ILock {
         // 获取锁
         String key = KEY_PREFIX + name;
         /**
-         * value值最好不要随便设，因为后面要判断哦，但是一般设置成一个有意义的值比较好！
-         * 同一个JVM内，线程ID是唯一的
+         * 线程标识：UUID + 线程ID
          */
-        long threadId = Thread.currentThread().getId();
-        String value = threadId + "";
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
         // 获取锁
         Boolean isSuccess = stringRedisTemplate.opsForValue()
-                .setIfAbsent(key, value, timeoutSec, TimeUnit.SECONDS);
+                .setIfAbsent(key, threadId, timeoutSec, TimeUnit.SECONDS);
         // 返回获取锁是否成功结果
         return BooleanUtil.isTrue(isSuccess);
     }
 
     @Override
     public void unlock() {
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        String key = KEY_PREFIX + name;
+        // 获取线程ID
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 获取缓存的线程标识
+        String cacheThreadId = stringRedisTemplate.opsForValue().get(key);
+        // 判断线程标识是否一致
+        if (threadId.equals(cacheThreadId)) {
+            // 释放锁
+            stringRedisTemplate.delete(key);
+        }
     }
 }
